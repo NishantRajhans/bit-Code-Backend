aaa29268a5c2e21ee5672d2c0dc5a14bf7f058f7
NishantRajhans <nishantrajhans35@gmail.com>
2024-07-13 00:15:47 +0530

    "add route"

--- Routes/WishListRoutes.js
+++ Routes/WishListRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router=express.Router();
 const {AuthMiddleware}=require("../Middleware/AuthMiddleWare")
-const {AddToDoneList,RemoveFromDoneList,AddToReviewList,RemoveFromReviewList}=require("../Controllers/WishListController");
+const {AddToDoneList,RemoveFromDoneList,AddToReviewList,RemoveFromReviewList,GetDoneList}=require("../Controllers/WishListController");
 router.post('/AddToDoneList',AuthMiddleware,AddToDoneList);
 router.post('/RemoveFromDoneList',AuthMiddleware,RemoveFromDoneList);
 router.post('/AddToReviewList',AuthMiddleware,AddToReviewList);
 router.post('/RemoveFromReviewList',AuthMiddleware,RemoveFromReviewList);
+router.post('/GetDoneList',AuthMiddleware,GetDoneList);
 module.exports = router;
