d295f153c9413cab37b2091e1903d2da5d7687fa
NishantRajhans <nishantrajhans35@gmail.com>
2024-07-12 23:06:49 +0530

    "change routes"

--- Routes/WishListRoutes.js
+++ Routes/WishListRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router=express.Router();
 const {AuthMiddleware}=require("../Middleware/AuthMiddleWare")
 const {AddToDoneList,RemoveFromDoneList,AddToReviewList,RemoveFromReviewList}=require("../Controllers/WishListController");
-router.get('/AddToDoneList',AuthMiddleware,AddToDoneList);
-router.get('/RemoveFromDoneList',AuthMiddleware,RemoveFromDoneList);
-router.get('/AddToReviewList',AuthMiddleware,AddToReviewList);
-router.get('/RemoveFromReviewList',AuthMiddleware,RemoveFromReviewList);
+router.post('/AddToDoneList',AuthMiddleware,AddToDoneList);
+router.post('/RemoveFromDoneList',AuthMiddleware,RemoveFromDoneList);
+router.post('/AddToReviewList',AuthMiddleware,AddToReviewList);
+router.post('/RemoveFromReviewList',AuthMiddleware,RemoveFromReviewList);
 module.exports = router;
